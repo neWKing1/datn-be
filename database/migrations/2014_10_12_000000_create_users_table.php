9c3efa6ac94ad706bbc84660e4061315dad3fc46
unknown <duonggiaothien@gmail.com>
2023-12-28 08:39:23 +0700

    add role user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('role', ['customer', 'staff', 'owner'])->default('customer');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
